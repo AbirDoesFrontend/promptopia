2430bfd70f1795a6b9534ad140c79dea50fa8e2e
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-07-18 23:56:20 +0600

    Navbar Done

---
+++ components/Nav.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () =>{
+      const response = await getProviders();
+
+      setProviders(response)
+    }
+
+    setProviders();
+  }, [])
+  
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href={"/"} className="flex gap-2 flex-center">
+        <Image
+          src={"/assets/images/logo.svg"}
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
